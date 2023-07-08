2c6ebfc2e8eedd35f8e392c9ec0e76a932ce1eff
SriMani-7 <basinenisrimanikanta@gmail.com>
2023-07-08 13:07:08 +0530

    splash screen

--- app/src/main/java/com/ithoughts/mynaa/tsd/rss/RssActivity.kt
+++ app/src/main/java/com/ithoughts/mynaa/tsd/rss/RssActivity.kt
@@ -3,6 +3,7 @@ package com.ithoughts.mynaa.tsd.rss
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.ithoughts.mynaa.tsd.rss.ui.MainNavigation
 import com.ithoughts.mynaa.tsd.ui.theme.TheSecretDairyTheme
@@ -10,6 +11,7 @@ import com.ithoughts.mynaa.tsd.ui.theme.TheSecretDairyTheme
 class RssActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             TheSecretDairyTheme(false) {
                 MainNavigation()
