5c6fb85ccf45a758dceaf867bf1d6f256394dd8a
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-03-25 17:24:54 +0530

    new theme and colors

--- app/src/main/java/srimani7/apps/feedfly/MainActivity.kt
+++ app/src/main/java/srimani7/apps/feedfly/MainActivity.kt
@@ -5,13 +5,13 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.core.view.WindowCompat
 import srimani7.apps.feedfly.core.design.TheSecretDairyTheme
 import srimani7.apps.feedfly.data.AppTheme
 import srimani7.apps.feedfly.navigation.URL_REGEX
@@ -24,6 +24,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val feedUrl: String? = addFeedFromShare()
+
+        enableEdgeToEdge()
+
         setContent {
             val appTheme by viewModel.settingsStateFlow.collectAsState()
             val isDarkTheme = isSystemInDarkTheme()
@@ -41,7 +44,7 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+
     }
 
     private fun addFeedFromShare(): String? {
