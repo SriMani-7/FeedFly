709c3562ead126c67007c19d4f726faf6a1c0457
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-07-04 11:01:06 +0530

    improved Label button of card and added more options menu

--- app/src/main/java/srimani7/apps/feedfly/ui/articles/LabelledArticleCard.kt
+++ app/src/main/java/srimani7/apps/feedfly/ui/articles/LabelledArticleCard.kt
@@ -4,14 +4,18 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -25,6 +29,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -87,19 +92,34 @@ fun LabelledArticleCard(
                 ArticleDescription(description = labelledArticle.description)
             }
 
-            TextButton(onClick = {
-                onChangeArticleLabel(
-                    labelledArticle.articleId,
-                    labelledArticle.labelId
-                )
-            }, modifier = Modifier.padding(4.dp)) {
-                Icon(
-                    painterResource(R.drawable.baseline_label_24), null, modifier = Modifier.size(
-                        ButtonDefaults.IconSize
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextButton(
+                    onClick = {
+                        onChangeArticleLabel(
+                            labelledArticle.articleId,
+                            labelledArticle.labelId
+                        )
+                    }, modifier = Modifier.padding(4.dp), colors = ButtonDefaults.textButtonColors(
+                        contentColor = MaterialTheme.colorScheme.onSecondaryContainer,
                     )
-                )
-                Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
-                Text(text = labelledArticle.label ?: "Add label")
+                ) {
+                    Icon(
+                        painterResource(R.drawable.baseline_label_24),
+                        null,
+                        modifier = Modifier.size(
+                            ButtonDefaults.IconSize
+                        )
+                    )
+                    Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+                    Text(text = labelledArticle.label ?: "Add label")
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.MoreVert, null)
+                }
             }
         }
         if (articleModalState.isVisible) {
@@ -115,7 +135,7 @@ fun LabelledArticleCard(
     }
     LaunchedEffect(Unit) {
         fromHtml(labelledArticle.description ?: "") {
-            if(descriptionUri == null) descriptionUri = it
+            if (descriptionUri == null) descriptionUri = it
             null
         }.toString()
     }
