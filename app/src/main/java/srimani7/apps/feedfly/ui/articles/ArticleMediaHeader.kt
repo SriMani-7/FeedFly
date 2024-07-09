b9398c6f17cb753313437e5f01c1314c38f13bbc
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-07-09 23:09:38 +0530

    ArticleMedia usages are reduced to :core:database module only

--- app/src/main/java/srimani7/apps/feedfly/ui/articles/ArticleMediaHeader.kt
+++ app/src/main/java/srimani7/apps/feedfly/ui/articles/ArticleMediaHeader.kt
@@ -5,30 +5,6 @@ import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import srimani7.apps.feedfly.core.database.entity.ArticleMedia
-
-@Composable
-fun ArticleMediaHeader(
-    articleMedia: ArticleMedia,
-    playAudio: (String) -> Unit
-) {
-    val imageSrc by rememberSaveable { mutableStateOf(articleMedia.url) }
-    val mediaType by rememberSaveable { mutableStateOf(articleMedia.urlType) }
-
-    when (mediaType) {
-        ArticleMedia.MediaType.IMAGE -> imageSrc?.let { ArticleImage(it) }
-        ArticleMedia.MediaType.AUDIO -> {
-            IconButton(onClick = { articleMedia.url?.let { playAudio(it) } }) {
-                Icon(Icons.Filled.PlayArrow, "Play")
-            }
-        }
-
-        else -> {}
-    }
-}
 
 @Composable
 fun ArticleMediaHeader(
