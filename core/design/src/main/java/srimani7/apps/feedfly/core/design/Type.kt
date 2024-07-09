c5fb1857c95613ee28963bfce985595e97649acf
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-07-09 12:04:19 +0530

    feat(typography): OpenSans font family in Typography

--- core/design/src/main/java/srimani7/apps/feedfly/core/design/Type.kt
+++ core/design/src/main/java/srimani7/apps/feedfly/core/design/Type.kt
@@ -2,33 +2,131 @@ package srimani7.apps.feedfly.core.design
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
+val openSans = FontFamily(
+    Font(R.font.opensans_bold, FontWeight.Bold),
+    Font(R.font.opensans_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.opensans_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.opensans_extra_bold_italic, FontWeight.ExtraBold, FontStyle.Italic),
+    Font(R.font.opensans_italic, style = FontStyle.Italic),
+    Font(R.font.opensans_light, FontWeight.Light),
+    Font(R.font.opensans_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.opensans_medium, FontWeight.Medium),
+    Font(R.font.opensans_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.opensans_regular),
+    Font(R.font.opensans_semi_bold, FontWeight.SemiBold),
+    Font(R.font.opensans_semi_bold_italic, FontWeight.SemiBold, FontStyle.Italic),
+)
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    labelLarge = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.10000000149011612.sp,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.5.sp,
+        lineHeight = 16.sp,
+        fontSize = 12.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = openSans,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+        letterSpacing = 0.5.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontSize = 11.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.5.sp,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.25.sp,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.4000000059604645.sp,
+        lineHeight = 16.sp,
+        fontSize = 12.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 40.sp,
+        fontSize = 32.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 36.sp,
+        fontSize = 28.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 32.sp,
+        fontSize = 24.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.W400,
+        letterSpacing = -0.25.sp,
+        lineHeight = 64.sp,
+        fontSize = 57.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 52.sp,
+        fontSize = 45.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 44.sp,
+        fontSize = 36.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 28.sp,
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.15000000596046448.sp,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.10000000149011612.sp,
+        lineHeight = 20.sp,
+        fontSize = 16.sp
+    ),
 )
