d3fed18f00430e72c9ab914851a8b7662b53314a
Sri Manikanta <84219628+SriMani-7@users.noreply.github.com>
2024-11-22 11:26:28 +0530

    notification channel for read later remainder

--- app/src/main/java/srimani7/apps/feedfly/FeedFlyApplication.kt
+++ app/src/main/java/srimani7/apps/feedfly/FeedFlyApplication.kt
@@ -1,7 +1,22 @@
 package srimani7.apps.feedfly
 
 import android.app.Application
+import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationManagerCompat
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class FeedFlyApplication: Application()
+class FeedFlyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val notificationChannelCompat = NotificationChannelCompat
+            .Builder(READ_LATER_NOTIFICATION_ID, NotificationManagerCompat.IMPORTANCE_HIGH)
+            .setName("Read later remainder").build()
+
+        NotificationManagerCompat.from(this).createNotificationChannel(notificationChannelCompat)
+    }
+
+    companion object {
+        const val READ_LATER_NOTIFICATION_ID = "feedfly_read_later"
+    }
+}
